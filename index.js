cdf2658d1246366a5bfc7c8c71e52f510df76001
Robert Voong <rvoong1408@gmail.com>
2026-05-16 14:58:02 +0200

    añadir .gitignore y api.js par ocultar API_KEY

---
+++ index.js
@@ -0,0 +1,7 @@
+
+import { API_KEY } from "./api.js";
+const tiempo = document.querySelector(".tiempo");
+const ciudad = document.querySelector(".inputCiudad");
+const cuadro = document.querySelector(".cuadro");
+
+console.log(API_KEY);
