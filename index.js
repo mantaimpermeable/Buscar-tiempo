8f5ea41c9edbb651e0c81530d7c70ef0058a1885
Robert Voong <rvoong1408@gmail.com>
2026-05-16 17:02:30 +0200

    logica terminada

--- index.js
+++ index.js
@@ -39,10 +39,68 @@ async function getTiempoInfo(ciudad){
     return await respuesta.json();
 }
 function displayInfo(datos){
-    
-}
-function getSimboloTiempo(IdTiempo){
+    //Sacamos valores de las constante de "datos" que es un json con toda la informacion
+    //scada de la api, para esto usamos la deestructuracion 
+    const {name: ciudad, 
+           main: {temp, humedad},
+           weather: [{descripcion,id}]} = datos;
 
+    //Hacemos que el cuadro sea visible y luego vamos añadiendo
+    cuadro.textContent="";
+    cuadro.style.display="flex";
+
+    const ciudadDisplay = document.createElement("h1");    
+    const temperaturaDisplay = document.createElement("p"); 
+    const humedadDisplay = document.createElement("p");     
+    const descripcionDisplay = document.createElement("p");    
+    const simboloDisplay = document.createElement("p");    
+
+    ciudadDisplay.textContent= ciudad;
+    temperaturaDisplay.textContent = `${(temp - 273.15).toFixed(1)}ºC`;
+    humedadDisplay.textContent = `Humedad: ${humedad}%`;
+    descripcionDisplay.textContent = descripcion;
+    simboloDisplay.textContent = getSimboloTiempo(id);
+
+    ciudadDisplay.classList.add("ciudad");
+    temperaturaDisplay.classList.add("temperatura");
+    humedadDisplay.classList.add("humedad");
+    descripcionDisplay.classList.add("descripcion");
+    simboloDisplay.classList.add("simboloTiempo");
+
+
+    cuadro.appendChild(ciudadDisplay);
+    cuadro.appendChild(temperaturaDisplay);
+    cuadro.appendChild(humedadDisplay);
+    cuadro.appendChild(descripcionDisplay);
+    cuadro.appendChild(simboloDisplay);
+
+
+    
+
+
+
+    }
+
+function getSimboloTiempo(IdTiempo){
+    switch(true){
+        case(IdTiempo >= 200 && IdTiempo < 300):
+        return "⛈️";
+        case(IdTiempo >= 300 && IdTiempo < 400):
+        return "🌧️";
+        case(IdTiempo >= 500 && IdTiempo < 600):
+        return "🌧️";
+        case(IdTiempo >= 600 && IdTiempo < 700):
+        return "❄️";
+        case(IdTiempo >= 700 && IdTiempo < 800):
+        return "😶‍🌫️";
+        case(IdTiempo === 800):
+        return "☀️";
+        case(IdTiempo >= 801 && IdTiempo < 810):
+        return "☁️";
+        default:
+            return "⁉️";
+        
+    }
 }
 function displayError(mensaje){
     const errorDisplay = document.createElement("p");
