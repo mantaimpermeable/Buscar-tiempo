10f667d4ed015f566df130e0f1f01dc6d226bc7e
Robert Voong <rvoong1408@gmail.com>
2026-05-16 15:52:30 +0200

    funciones y caso sin ciudad

--- index.js
+++ index.js
@@ -1,7 +1,39 @@
-
+//Creamos otro archivo .js donde exportamos la variable. API_KEY porque no me fucnionan
+//las librerias para usar .env
 import { API_KEY } from "./api.js";
 const tiempo = document.querySelector(".tiempo");
-const ciudad = document.querySelector(".inputCiudad");
+const ciudadInput = document.querySelector(".inputCiudad");
 const cuadro = document.querySelector(".cuadro");
 
-console.log(API_KEY);
+tiempo.addEventListener("submit", event => {
+    event.preventDefault();
+
+    const ciudad = ciudadInput.value;
+
+    if(ciudad){
+
+    }
+    else{
+        displayError("Introduzca una ciudad");
+    }
+});
+
+async function getTiempoInfo(ciudad){
+
+}
+function displayInfo(datos){
+
+}
+function getSimboloTiempo(IdTiempo){
+
+}
+function displayError(mensaje){
+    const errorDisplay = document.createElement("p");
+    errorDisplay.textContent = mensaje;
+    errorDisplay.classList.add("error");
+
+    cuadro.textContent="";
+    cuadro.style.display = "flex";
+    cuadro.appendChild(errorDisplay);
+
+}
